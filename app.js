54856062cba87e4256ba4d6e0ef594c7a8979a6f
Sander <smk012026@gmail.com>
2026-02-02 13:58:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed"
+}
+
+//Under definerer jeg en variabel og endrer den ved å legge til en til variabel
+let x = 5;
+let y = x + 8;
+
+const resultat = x+y;
+
+// Her sier jeg at i mitt html element som har #id resultatTest, så skal innholder byttes ut med minst const "resultat", i min HTML bestemmer jeg også at dette skal skje "onclick"
+function myFunction2() {
+    document.getElementById("resultatTest").innerHTML= resultat;
+}
+
+let statement1, statement2, statement3;
+statement1 = "Dette er første mulighet";
+statement2 = "Dette er andre mulighet";
+statement3 = "Dette er tredje mulighet";
+
+function stateFunc1() {
+    document.getElementById("statements").innerHTML = statement1;
+}
+
+function stateFunc2() {
+    document.getElementById("statements").innerHTML = statement2;
+}
+
+function stateFunc3() {
+    document.getElementById("statements").innerHTML = statement3;
+}
+
+
+
+
+
